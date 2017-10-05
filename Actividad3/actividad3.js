80d23a9d7b045e3bd807caf5efcfa29be41cd129
sebas <juansebastiantorresriaga@gmail.com>
2017-10-05 09:58:47 +0200

    Entrega actividad 3

---
+++ Actividad3/actividad3.js
@@ -0,0 +1,13 @@
+var num = 44
+var num1 = 0
+
+var num1 = prompt ("Pon un numero");
+
+if (num > num1) {
+  alert ("Casi lo consigues")
+  console.log();
+} else if (num < num1) {
+  alert ("Te pasaste boludo")
+} else {
+  alert ("Enhorabuena shampion!")
+}
