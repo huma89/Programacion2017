f8cbee3ae69396832d930ae8b04303df7fa27c90
sebas <juansebastiantorresriaga@gmail.com>
2017-10-05 11:10:32 +0200

    Entrega actividad

---
+++ proyectoahorcado/ahorcado.js
@@ -0,0 +1,15 @@
+//Tener almacenada una letra la cual deberemos acertar
+//Tener otra variable que almacenará la variable pedida por el usuario.
+//Comparar ambas letras y dando un mensaje de acertado o no.
+
+var letra1 = "a";
+var letra3 = "A";
+var letra2 = prompt("Introduce una letra nano!" , "");
+
+if (letra2 == letra1) {
+  alert ("Has acertado shampion!");
+}else if (letra2 == letra3){
+  alert ("Has acertado shampion!");
+}else {
+  alert ("Prueba otra vez noob!");
+}
